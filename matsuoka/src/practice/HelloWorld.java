1f7c2b89d3b2c1d73c6ac103d1aace337f5356bd
まつおか <matsuoka@example.com>
2024-01-16 11:27:39 +0900

    こみっとのれんしゅう

--- matsuoka/src/practice/HelloWorld.java
+++ matsuoka/src/practice/HelloWorld.java
@@ -3,7 +3,6 @@ package practice;
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("hello, world.");
-    System.out.println("hello, world.");
-    System.out.println("hello, world.");
+    System.out.println("Git特訓中.");
   }
 }
