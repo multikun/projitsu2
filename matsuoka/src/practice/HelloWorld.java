0184fab64fe08f71445d04c0487f6ceda555c371
kyazumix <kyazumix@example.com>
2024-01-16 11:50:11 +0900

    コミット練習

--- matsuoka/src/practice/HelloWorld.java
+++ matsuoka/src/practice/HelloWorld.java
@@ -2,8 +2,8 @@ package practice;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("hello, world.");
-    System.out.println("hello, world.");
-    System.out.println("hello, world.");
+    System.out.println("hello, tomohiro.");
+    System.out.println("hello, hello.");
+    System.out.println("world, world.");
   }
 }
